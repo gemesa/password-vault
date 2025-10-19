09756b05ddb858d00fc70e916af4a3f941be068e
Andras Gemes <gemesa@protonmail.com>
2025-10-19 10:53:24 +0200

    refactor: remove unnecessary code and comment

--- password-vault/PasswordVaultApp.swift
+++ password-vault/PasswordVaultApp.swift
@@ -1,17 +1,7 @@
-//
-//  password_vaultApp.swift
-//  password-vault
-//
-//  Created by András Gémes on 2025. 10. 17..
-//
-
 import SwiftUI
 
 @main
 struct password_vaultApp: App {
-    init() {
-        
-    }
     var body: some Scene {
         WindowGroup {
             ContentView()
